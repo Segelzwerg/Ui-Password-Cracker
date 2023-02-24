dcc692746cb8ae98f3ea31cfbb9982382428bc84
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2023-02-24 21:17:45 +0100

    ADD: event types
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- event/event.py
+++ event/event.py
@@ -1,7 +1,36 @@
+import logging
+from abc import abstractmethod
+from time import sleep
+
+import keyboard
+
+
 class Event:
-    def __init__(self, event_type, event) -> None:
-        self._type = event_type
+    def __init__(self, event) -> None:
         self._event = event
 
+    @abstractmethod
+    def perform(self):
+        raise NotImplementedError()
 
-ENTER = Event('keyboard', 'enter')
+
+class KeyboardEvent(Event):
+    def perform(self):
+        keyboard.press_and_release(self._event)
+        logging.debug(f'{self._event} pressed')
+
+
+class MouseEvent(Event):
+    pass
+
+
+class TimeEvent(Event):
+    def perform(self):
+        sleep(self._event)
+        logging.debug(f'{self._event}s waited')
+
+
+ENTER = KeyboardEvent('enter')
+WAIT_HALF_SEC = TimeEvent(0.5)
+WAIT_SEC = TimeEvent(1)
+WAIT_2_SEC = TimeEvent(2)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from detect_field import find_field
+from event.event import ENTER, WAIT_HALF_SEC
 from typer import write
 
 
@@ -10,6 +11,9 @@ def main():
     password_location = find_field()
     logging.debug(f'Password field location: {password_location}')
     write('password', password_location)
+    ENTER.perform()
+    WAIT_HALF_SEC.perform()
+    ENTER.perform()
 
 
 if __name__ == '__main__':
