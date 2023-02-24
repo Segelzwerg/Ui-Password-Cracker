503ab11ef3c69baef1766819d77efe04785b946c
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2023-02-24 23:13:41 +0100

    ADD: arguments to main
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 from detect_field import find_field
 from event.event import ENTER, WAIT_HALF_SEC
@@ -7,17 +8,18 @@ from typer import write
 from worldlist.generate import create
 
 
-def main():
+def main(regex: str) -> None:
     logging.basicConfig(level=logging.DEBUG)
     logging.info('Select the password field by right clicking on it.')
     password_location = find_field()
     logging.debug(f'Password field location: {password_location}')
     confirm_list = [ENTER, WAIT_HALF_SEC, ENTER]
-    word_list = create('S|s')
+    word_list = create(regex)
     for word in word_list:
         write(word, password_location)
         play(confirm_list)
 
 
 if __name__ == '__main__':
-    main()
+    args = sys.argv[1:]
+    main(args[0])
