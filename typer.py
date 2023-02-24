328b284e83f35994839492aa4f68faf74c314321
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2023-02-24 22:56:04 +0100

    TEMP FIX: multiple inputs
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- typer.py
+++ typer.py
@@ -1,3 +1,4 @@
+import logging
 from time import sleep
 from typing import Tuple
 
@@ -9,5 +10,8 @@ def write(word: str, position: Tuple[int, int]) -> None:
     keyboard._os_keyboard.init()  # https://github.com/boppreh/keyboard/issues/446#issuecomment-922032100
     sleep(0.1)
     mouse.move(position[0], position[1])
-    mouse.click()
+    mouse.click(button=mouse.LEFT)
+    keyboard.press_and_release('tab')  # dirty fix
+    sleep(0.1)
     keyboard.write(word)
+    logging.debug(f'{word} written')
