0353492a2d4086d270c2f2d90afd36bd2eb959b6
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2023-02-24 22:56:48 +0100

    UPDATE: use a wordlist
    Signed-off-by: Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from detect_field import find_field
 from event.event import ENTER, WAIT_HALF_SEC
 from event.player import play
 from typer import write
+from worldlist.generate import create
 
 
 def main():
@@ -12,7 +13,7 @@ def main():
     password_location = find_field()
     logging.debug(f'Password field location: {password_location}')
     confirm_list = [ENTER, WAIT_HALF_SEC, ENTER]
-    word_list = ['password']
+    word_list = create('S|s')
     for word in word_list:
         write(word, password_location)
         play(confirm_list)
